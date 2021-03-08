c9214339752d3f2f4f48ddf27c51d961b7b7d785
ConnectedSystems <takuyai@gmail.com>
2021-03-09 01:18:50 +1100

    Cleaned up example file

--- examples/network_creation.jl
+++ examples/network_creation.jl
@@ -1,11 +1,5 @@
 using YAML
-using LightGraphs, MetaGraphs
-using DataFrames, CSV
-
-import ModelParameters: update, update!, Model
 using Streamfall
-
-using Infiltrator
 
 
 network = YAML.load_file("../test/data/campaspe/campaspe_network.yml")
@@ -14,18 +8,3 @@ g, mg = create_network("Example Network", network)
 inlets, outlets = find_inlets_and_outlets(g)
 
 @info "Network has the following inlets and outlets:" inlets outlets
-
-
-
-# climate_data = DataFrame!(CSV.File("../test/data/climate/climate_historic.csv", 
-#                           comment="#",
-#                           dateformat="YYYY-mm-dd"))
-# climate = Climate(climate_data, "_rain", "_evap")
-
-# @info "Running example stream..."
-# timesteps = sim_length(climate)
-# for ts in (1:timesteps)
-#     for outlet in outlets
-#         run_node!(mg, g, outlet, climate, ts)
-#     end
-# end
---
+++ examples/run_nodes.jl
@@ -0,0 +1,22 @@
+using YAML
+using DataFrames, CSV
+
+using Streamfall
+
+
+HERE = @__DIR__
+
+include(HERE*"/network_creation.jl")
+
+climate_data = DataFrame!(CSV.File("../test/data/campaspe/climate/climate_historic.csv", 
+                          comment="#",
+                          dateformat="YYYY-mm-dd"))
+climate = Climate(climate_data, "_rain", "_evap")
+
+@info "Running example stream..."
+timesteps = sim_length(climate)
+for ts in (1:timesteps)
+    for outlet in outlets
+        run_node!(mg, g, outlet, climate, ts)
+    end
+end
