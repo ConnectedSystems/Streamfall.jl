ad67c1c4b2f466619431352a59d541c621815e0d
ConnectedSystems <takuyai@gmail.com>
2021-03-26 13:50:46 +1100

    Updated example to use run_catchment

--- examples/run_nodes.jl
+++ examples/run_nodes.jl
@@ -1,31 +1,23 @@
-using YAML
-using DataFrames, CSV
-
-using Streamfall
+using YAML, DataFrames, CSV, Plots
+using MetaGraphs, Streamfall
 
 
-HERE = @__DIR__
+# Load and generate stream network
+network = YAML.load_file("../test/data/campaspe/campaspe_network.yml")
+g, mg = create_network("Example Network", network)
 
-include(HERE*"/network_creation.jl")
-
+# Load climate data
 climate_data = DataFrame!(CSV.File("../test/data/campaspe/climate/climate_historic.csv", 
                           comment="#",
                           dateformat="YYYY-mm-dd"))
 climate = Climate(climate_data, "_rain", "_evap")
 
 @info "Running example stream..."
-timesteps = sim_length(climate)
-for ts in (1:timesteps)
-    for outlet in outlets
-        run_node!(mg, g, outlet, climate, ts)
-    end
-end
+run_catchment!(mg, g, climate)
 
-using MetaGraphs, Plots
+inlets, outlets = find_inlets_and_outlets(g)
 
 out = outlets[1]
 out_node = get_prop(mg, out, :node)
-
-
 
 plot(out_node.outflow)
