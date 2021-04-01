d9dbf61537d4d216da1c0a56cf2923e3634a1c31
ConnectedSystems <takuyai@gmail.com>
2021-04-01 22:36:00 +1100

    Update run_node example

--- examples/run_nodes.jl
+++ examples/run_nodes.jl
@@ -7,7 +7,7 @@ network = YAML.load_file("../test/data/campaspe/campaspe_network.yml")
 g, mg = create_network("Example Network", network)
 
 # Load climate data
-climate_data = DataFrame!(CSV.File("../test/data/campaspe/climate/climate_historic.csv", 
+climate_data = DataFrame!(CSV.File("../test/data/campaspe/climate/climate_fortran.csv", 
                           comment="#",
                           dateformat="YYYY-mm-dd"))
 climate = Climate(climate_data, "_rain", "_evap")
@@ -17,7 +17,21 @@ run_catchment!(mg, g, climate)
 
 inlets, outlets = find_inlets_and_outlets(g)
 
-out = outlets[1]
-out_node = get_prop(mg, out, :node)
+in_node = get_prop(mg, inlets[1], :node)
 
-plot(out_node.outflow)
+plot(in_node.outflow)
+
+# outflow = in_node.outflow
+# append!(outflow, NaN)
+# erain = in_node.effective_rainfall
+# append!(erain, NaN)
+
+# res = Dict(
+#     "Outflow"=> outflow,
+#     "CMD"=>in_node.storage,
+#     "Quick Store"=>in_node.quick_store,
+#     "Slow Store"=>in_node.slow_store,
+#     "erain"=>erain
+# )
+
+# CSV.write("outflow.csv", DataFrame(res))
