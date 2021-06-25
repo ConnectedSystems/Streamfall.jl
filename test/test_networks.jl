7c98b9e4cac1777a844143b13ac1cafbe431eb1a
ConnectedSystems <takuyai@gmail.com>
2021-06-25 10:17:31 +1000

    Separated spec reloading and calibration tests

--- test/test_networks.jl
+++ test/test_networks.jl
@@ -19,7 +19,7 @@ end
 
 
 @testset "Saving a network spec" begin
-    spec::Dict{String, Union{Dict{String, Any}, Any}} = Streamfall.extract_network_spec(sn)
+    spec = Streamfall.extract_network_spec(sn)
     @test spec isa Dict
     @test haskey(spec, "leaf_river")
 
@@ -30,18 +30,49 @@ end
     tmp_fn, io = mktemp()
     @test Streamfall.save_network_spec(tmp_sn, tmp_fn) isa Any
 
+    # Ensure reloaded network is usable
     tmp_network = YAML.load_file(tmp_fn)
-    tmp_sn = create_network("Test", tmp_network)
+    new_sn = create_network("Test", tmp_network)
 
-    @test tmp_sn isa Streamfall.StreamfallNetwork
+    @test new_sn isa Streamfall.StreamfallNetwork
 
     # There is currently a bug in YAML.jl which parses string integers as Ints
     # which causes issues when the value starts with a 0 
     # (incorrectly parses as an octal integer)
     # If this test does not throw an error, then it means the issue has been resolved
     # and can be removed after checking.
-    # see:
-    # https://github.com/JuliaData/YAML.jl/pull/45
-    @test_throws ErrorException sn["02473000"]
+    # see: https://github.com/JuliaData/YAML.jl/pull/45
+    @test_throws ErrorException new_sn["02473000"]
 end
 
+
+@testset "Reloading a network spec" begin
+    spec::Dict{String, Union{Dict{String, Any}, Any}} = Streamfall.extract_network_spec(sn)
+    @test spec isa Dict
+    @test haskey(spec, "leaf_river")
+
+    tmp_sn = create_network("Test", spec)
+
+    tmp_fn, io = mktemp()
+    @test Streamfall.save_network_spec(tmp_sn, tmp_fn) isa Any
+
+    # Ensure reloaded network is usable
+    tmp_network = YAML.load_file(tmp_fn)
+    new_sn = create_network("Test", tmp_network)
+
+    @test new_sn isa Streamfall.StreamfallNetwork
+
+    # Load climate data
+    date_format = "YYYY-mm-dd"
+    obs_data = DataFrame!(CSV.File(joinpath(DATA_PATH, "leaf_river_data.csv"),
+                            comment="#",
+                            dateformat=date_format))
+
+    hist_streamflow = obs_data[:, ["Date", "leaf_river_outflow"]]
+    climate_data = obs_data[:, ["Date", "leaf_river_P", "leaf_river_ET"]]
+    climate = Climate(climate_data, "_P", "_ET")
+
+    # Ensure reloaded spec still calibrates
+    metric = (obs, sim) -> 1.0 - Streamfall.NNSE(obs, sim)
+    @test calibrate!(new_sn, climate, hist_streamflow; metric=metric, MaxTime=10) isa Any
+end
