ae9a49f1b587aa7da17e14a20cf4c2d25b2d322d
ConnectedSystems <takuyai@gmail.com>
2021-09-07 14:03:18 +1000

    Initial docstring for GR4J

--- src/GR4JNode.jl
+++ src/GR4JNode.jl
@@ -5,6 +5,28 @@ using ModelParameters
 abstract type GRNJNode <: NetworkNode end
 
 
+"""
+GR4J Node
+
+A four-parameter model with two stores.
+
+# Parameters
+- x1 : maximum capacity of the production store (mm) (> 0)
+- x2 : groundwater exchange coefficient (mm) (value < and > 0 possible)
+- x3 : one day ahead maximum capacity of the routing store (mm, > 0)
+- x4 : time base of unit hydrograph UH1 (days, > 0.5)
+
+# References
+1. Perrin, C., Michel, C., Andréassian, V., 2003. 
+    Improvement of a parsimonious model for streamflow simulation. 
+    Journal of Hydrology 279, 275–289. 
+    https://doi.org/10.1016/S0022-1694(03)00225-7
+
+2. MacDonald, A. 2014. 
+    Python GR4J
+    https://github.com/amacd31/gr4j
+
+"""
 Base.@kwdef mutable struct GR4JNode{A <: Union{Param, Real}} <: GRNJNode
     @network_node
 
