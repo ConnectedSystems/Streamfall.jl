ce2955bf8a292b8657bbb20d673010e4324056e5
ConnectedSystems <takuyai@gmail.com>
2021-09-13 19:22:00 +1000

    Add license agreement to GR4J implementation

--- src/GR4JNode.jl
+++ src/GR4JNode.jl
@@ -1,3 +1,41 @@
+"""
+Portions of the GR4J implementation is directly adapted from Python code written by Andrew MacDonald (2014).
+
+Per license requirements, the full license conditions included below.
+
+
+Copyright (c) 2014, Andrew MacDonald
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+    * The name of the author may not be used to
+    endorse or promote products derived from this software without
+    specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
 using Parameters
 using ModelParameters
 
@@ -288,8 +326,8 @@ function run_gr4j(P, E, X1, X2, X3, X4, area, p_store=0.0, r_store=0.0)::Tuple
     QR = r_store - R2
     r_store = R2
     QD = max(0.0, UH2[1]*0.1+groundwater_exchange)
+
     Q = (QR + QD) * area
-    # (QR + QD) * area * 1000.0/86400.0  # m³/sec
 
     return Q, p_store, r_store, UH1, UH2
 end
