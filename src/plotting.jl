8f2d7ff42c9419159358763d741fea1b1835a98b
ConnectedSystems <takuyai@gmail.com>
2021-09-20 15:52:18 +1000

    Expand convenience plot capabilities

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,4 +1,5 @@
 using Plots, StatsPlots
+using Plots.Measures
 
 
 function quickplot(node::NetworkNode)
@@ -26,15 +27,47 @@ function quickplot(obs, node::NetworkNode, climate::Climate, label=""; burn_in=1
 end
 
 
-function quickplot(obs::Array, sim::Array, xticklabels::Array, label="")
+function quickplot(obs::Array, sim::Array, xticklabels::Array, label="Modeled", log=true; metric=Streamfall.mKGE)
     @assert length(xticklabels) == length(obs) || "x-axis tick label length and observed lengths do not match!"
     @assert length(xticklabels) == length(sim) || "x-axis tick label length and simulated lengths do not match!"
-    
-    fig = plot(xticklabels, obs, label="Historic")
-    plot!(xticklabels, sim, label=label)
-    qqfig = qqplot(obs, sim, legend=false, markerstrokewidth=0)
 
-    combined = plot(fig, qqfig, size=(800, 400))
+    score = round(metric(obs, sim), digits=4)
+    metric_name = String(Symbol(metric))
+
+    label = "$(label) ($(metric_name): $(score))"
+    fig = plot(xticklabels, obs, label="Observed", legend=:best, ylabel="Streamflow", xlabel="Date")
+    plot!(xticklabels, sim, label=label, alpha=0.7)
+    qqfig = qqplot(obs, sim, legend=false, markerstrokewidth=0, alpha=0.7, xlabel="Observed", ylabel="Modeled")
+
+    if log
+        xaxis!(qqfig, :log)
+        yaxis!(qqfig, :log)
+    end
+
+    combined = plot(fig, qqfig, size=(800, 400), left_margin=5mm)
 
     return combined
+end
+
+
+"""
+    plot_residuals(obs::Array, sim::Array; xlabel="", ylabel="", title="")
+
+Plot residual between two sequences.
+
+# Arguments
+- x : x-axis data
+- y : y-axis data
+- xlabel : x-axis label
+- ylabel : y-axis label
+- title : title text
+"""
+function plot_residuals(x::Array, y::Array; xlabel="", ylabel="", title="")
+    # 1:1 Plot
+    fig_1to1 = scatter(x, y, legend=false,
+                       markerstrokewidth=0, markerstrokealpha=0, alpha=0.2)
+    plot!(x, y, color=:red, markersize=0.1, markerstrokewidth=0,
+    xlabel=xlabel, ylabel=ylabel, title=title)
+
+    return fig_1to1
 end
