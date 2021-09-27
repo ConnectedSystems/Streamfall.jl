fa34709027702f5526fcc199bcbfff270c36fdce
ConnectedSystems <takuyai@gmail.com>
2021-09-27 18:33:01 +1000

    Add temporal cross-section plot
    Needs better name...

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,5 +1,6 @@
 using Plots, StatsPlots
 using Plots.Measures
+using DataFrames, Dates, Statistics
 
 
 function quickplot(node::NetworkNode)
@@ -80,10 +81,64 @@ function plot_residuals(x::Array, y::Array; xlabel="", ylabel="", title="")
 end
 
 
-# mass comparison plot
-# """
-# Produce a scatter plot from a DataFrame of scores.
-# """
-# function mass_plot(df)
+"""
+    temporal_cross_section(dates, obs, sim; ylabel=nothing, func::Function=Streamfall.ME, period::Function=month)
 
-# end
+Provides indication of predictive uncertainty across time, grouped by `period`.
+
+Notes:
+Assumes daily data.
+Filters out leap days.
+
+# Arguments
+- dates : Date of each observation
+- obs : observed data
+- sim : modeled results
+- ylabel : Optional replacement ylabel. Uses name of `func` if not provided.
+- `func::Function` : Function to apply to each month-day grouping
+- `period::Function` : Method from `Dates` package to group (defaults to `month`)
+"""
+function temporal_cross_section(dates, obs, sim; ylabel=nothing, func::Function=Streamfall.ME, period::Function=month)
+    df = DataFrame(Date=dates, Observed=obs, Modeled=sim)
+    sp = sort(unique(period.(dates)))
+
+    # Remove leap days (when using monthday)
+    deleteat!(sp, findall(x -> x == (2,29), sp))
+
+    x_section = fill(0.0, length(sp))
+    min_section = fill(0.0, length(sp))
+    max_section = fill(0.0, length(sp))
+    med_section = fill(0.0, length(sp))
+    for (i, obs_i) in enumerate(sp)
+        obs_g = df[in([obs_i]).(period.(df.Date)), :]
+        obs_gi = obs_g.Observed
+        sim_gi = obs_g.Modeled
+
+        tmp = func.([[x] for x in obs_gi], [[x] for x in sim_gi])
+        min_section[i] = minimum(tmp)
+        max_section[i] = maximum(tmp)
+        med_section[i] = median(tmp)
+        x_section[i] = mean(tmp)
+    end
+
+    if isnothing(ylabel)
+        ylabel = nameof(func)
+    end
+
+    xlabels = join.(sp, "-")
+    fig = plot(xlabels, x_section,
+               ribbon=(x_section .- min_section, max_section .- x_section),
+               label="Mean $(ylabel)",
+               xlabel=nameof(period),
+               legend=:bottomleft,
+               legendfont=Plots.font(12),
+               fg_legend=:transparent,
+               bg_legend=:transparent,
+               ylabel=ylabel,
+               left_margin=5mm,
+               bottom_margin=5mm,
+               size=(850, 400))
+    scatter!(xlabels, med_section, markerstrokewidth=0, markersize=4, alpha=0.4, label="Median")
+
+    return fig
+end
