0a40238f6eea58d5db3d0b51ff6f7c9a3b087a6a
ConnectedSystems <takuyai@gmail.com>
2021-12-30 12:15:22 +1100

    Reorganize temporal cross section support to leverage a struct

--- src/Analysis/uncertainty.jl
+++ src/Analysis/uncertainty.jl
@@ -1,213 +1,144 @@
 using Dates
 using Statistics, DataFrames, Bootstrap
+using OrderedCollections
 import ..ME, ..MAE
 
 
-"""
-Conduct temporal uncertainty analysis on observations.
+mutable struct TemporalCrossSection{A<:Dates.Date, B<:Real, D<:OrderedDict}
+    dates::Array{A}
+    ts::Array{B}
+    subperiods::D
+    cross_section::Union{DataFrame, Nothing}
+end
 
-Calculates the standard boxplot values for given subperiods (median, -1.5*IQR, +1.5*IQR).
 
-Notes: Ignores leap days when using monthday subperiods.
-
-# Returns
-tuple: 
-     - x_section : Median of observations for a given period (default: day of year)
-     - low_section : Lower 95 percentile interval bound of metric value for period
-     - upp_section : Upper 95 percentile interval bound of metric value for period
-     - whisker_range : Mean range indicated by (upp_section - low_section)
-"""
-function temporal_uncertainty(dates, obs, period::Function)
-    df = DataFrame(Date=dates, Observed=obs)
+function TemporalCrossSection(dates::Array, ts::Array, period::Function=monthday)
+    df = DataFrame(Date=dates, data=ts)
     sp = sort(unique(period.(dates)))
 
     # Remove leap days (when using monthday)
     deleteat!(sp, findall(x -> x == (2,29), sp))
 
-    x_section = fill(0.0, length(sp))
-    min_section = fill(0.0, length(sp))
-    max_section = fill(0.0, length(sp))
-    low_section = fill(0.0, length(sp))
-    upp_section = fill(0.0, length(sp))
-    for (i, obs_i) in enumerate(sp)
-        obs_g = df[in([obs_i]).(period.(df.Date)), :]
-        obs_gi = obs_g.Observed
+    res = OrderedDict(
+        sp_i => df[period.(df.Date) .== [sp_i], :].data for sp_i in sp
+    )
 
-        ab_min, lower_q, upper_q, ab_max = quantile(obs_gi, [0.0, 0.025, 0.975, 1.0])
-        low_section[i] = lower_q
-        upp_section[i] = upper_q
-        min_section[i] = ab_min
-        max_section[i] = ab_max
-        # mae_section[i] = MAE(obs_gi, sim_gi)
-        x_section[i] = median(obs_gi)
-    end
-
-    whisker_range = round(mean(upp_section .- low_section), digits=2)
-
-    # diff_x = diff(x_section)
-    # normed_diff = (diff_x .- mean(diff_x)) ./ std(diff_x)
-    # roughness = mean(diff(normed_diff).^2 ./ 4)  # 0 = smooth, 1 = maximal roughness
-    # mean_mad = mean(mad_section)
-
-    cv = std(whisker_range) / mean(whisker_range)
-    return x_section, low_section, upp_section, whisker_range, cv
+    return TemporalCrossSection(dates, ts, res, nothing)
 end
 
 
 """
-Conduct temporal uncertainty analysis on observations.
-
-Calculates the standard boxplot values for given subperiods (median, -1.5*IQR, +1.5*IQR).
-
-Periods of high uncertainty, defined as 1.5 std from mean tolerance interval by default,
-are given weights > 1.0, increasing with distance. Those within this interval is given lesser weight.
-
-Notes: Ignores leap days when using monthday periods.
-
-# Returns
-tuple: 
-     - x_section : Median of observations for a given period (default: day of year)
-     - low_section : Lower 95-percentile interval bound of metric value for period
-     - upp_section : Upper 95-percentile interval bound of metric value for period
-     - whisker_range : Mean range indicated by max_section - min_section
-     - weighted : weights (0 to 1) indicating relative to `threshold * std(x_section)`
+TemporalCrossSection constructor that handles two time series
 """
-function temporal_uncertainty(dates, obs; period::Function=monthday, min_weight::Float64=0.3)
+function TemporalCrossSection(dates::Array, obs::Array, sim::Array,
+                              period::Function=monthday)
+    Y = ME.(obs, sim)
+    return TemporalCrossSection(dates, Y, period)
+end
 
-    x_section, low_section, upp_section, whisker_range, cv_r = temporal_uncertainty(dates, obs, period)
 
-    # use min-max scaling to indicate where to place greater weight
-    weights = 1 .+ (whisker_range .- minimum(whisker_range)) ./ (maximum(whisker_range) - minimum(whisker_range))
-    weights[weights .< (1.0 + min_weight)] .= (1.0 + min_weight)
+function cross_section(tr::TemporalCrossSection)
+    sp = tr.subperiods
 
-    weighted = fill(1.0, length(obs))
+    # Array holding boxplot-like data
+    # No. of Dims: (75, 95) * (2) + median + min/max = 7
+    n_cols = 7  # -q95, -q75, q50, q75, q95, min, max 
+    boxframe = fill(0.0, length(keys(sp)), n_cols)
+
+    target_pts = [0.0, 0.025, 0.125, 0.875, 0.975, 1.0]
+    for (i, (ki, vi)) in enumerate(sp)
+        ab_min, lower_95, lower_75, upper_75, upper_95, ab_max = quantile(vi, target_pts)
+
+        boxframe[i, 1:3] .= ab_min, lower_95, lower_75
+        boxframe[i, 4] = median(vi)
+        boxframe[i, 5:7] .= upper_75, upper_95, ab_max
+    end
+
+    cols = [:abs_min, :lower_95, :lower_75, :median, 
+            :upper_75, :upper_95, :abs_max]
+
+    x_section = DataFrame(boxframe, cols)
+    x_section[:, :subperiod] = collect(keys(sp))
+
+    tr.cross_section = x_section
+
+    return x_section
+end
+
+
+"""
+    offsets(tr::TemporalCrossSection, period::Function=monthday)
+
+Determine (median) offsets, assuming mean error-based temporal cross-sectional analysis 
+was performed. Only supports daily time series.
+"""
+function offsets(tr::TemporalCrossSection)
+    x_section = tr.cross_section
+    offset = x_section.median
+    dates = tr.dates
+
+    offset_vals = zeros(length(dates))
+
     doy = Dates.dayofyear.(dates)
-    mds = Dates.monthday.(dates)
+    mds = monthday.(dates)
     for (idx, (d, md)) in enumerate(zip(doy, mds))
-        if md == (2, 29)
-            weighted[idx] = weights[d+1]
-            continue
-        elseif d == 366
-            weighted[idx] = weights[d-1]
+        if md == (2, 29) || d == 366
+            offset_vals[idx] = offset[d-1] * -1.0
             continue
         end
 
-        weighted[idx] = weights[d]
+        offset_vals[idx] = offset[d] * -1.0
     end
 
-    return x_section, low_section, upp_section, whisker_range, weighted, cv_r
+    return offset_vals
 end
 
 
-"""
-Conduct temporal uncertainty analysis on simulated results relative to observations
-using the provided function `func`.
-
-Calculates the standard boxplot values for given subperiods (median, -1.5*IQR, +1.5*IQR).
-
-Notes: Ignores leap days when using monthday subperiods.
-
-# Returns
-tuple: 
-     - x_section : Median of observations for a given period (default: day of year)
-     - low_section : Lower 95 percentile interval bound for a given period
-     - upp_section : Upper 95 percentile interval bound for a given period
-     - min_section : minimum score for given period
-     - max_section : maximum score for given period
-     - whisker_range : Mean range indicated by q0.975 - q0.025 (i.e., 95 percentile confidence interval)
-     - cv_r : coefficient of variation for whisker range. Indicates variability of 
-"""
-function temporal_uncertainty(dates, obs, sim, period::Function, func::Function)
-    df = DataFrame(Date=dates, Observed=obs, Modeled=sim)
-    sp = sort(unique(period.(dates)))
-
-    # Remove leap days (when using monthday)
-    deleteat!(sp, findall(x -> x == (2,29), sp))
-
-    x_section = fill(0.0, length(sp))
-    min_section = fill(0.0, length(sp))
-    max_section = fill(0.0, length(sp))
-    low_section = fill(0.0, length(sp))
-    upp_section = fill(0.0, length(sp))
-    # mae_section = fill(0.0, length(sp))
-    for (i, obs_i) in enumerate(sp)
-        obs_g = df[in([obs_i]).(period.(df.Date)), :]
-        obs_gi = obs_g.Observed
-        sim_gi = obs_g.Modeled
-
-        tmp = func.([[x] for x in obs_gi], [[x] for x in sim_gi])
-
-        min_tmp, lower_q, med, upper_q, max_tmp = quantile(tmp, [0.0, 0.025, 0.5, 0.975, 1.0])
-
-        low_section[i] = lower_q
-        upp_section[i] = upper_q
-        min_section[i] = min_tmp
-        max_section[i] = max_tmp
-        # mae_section[i] = MAE(obs_gi, sim_gi)
-        x_section[i] = med
-    end
-
-    whisker_range = upp_section .- low_section
-
-    # diff_x = diff(x_section)
-    # normed_diff = (diff_x .- mean(diff_x)) ./ std(diff_x)
-    # roughness = mean(diff(normed_diff).^2 ./ 4)  # 0 = smooth, 1 = maximal roughness
-    cv_r = std(whisker_range) / mean(whisker_range)
-
-    return x_section, low_section, upp_section, min_section, max_section, whisker_range, cv_r, stderror(whisker_range)
-end
-
-
-"""
-Conduct temporal uncertainty analysis on observations.
-
-Calculates boxplot-like values for given subperiods (mean, q0.025, q0.975).
-
-A temporal cross-section is taken for each temporal period (default: month-day)
-and greater weights are placed on periods of greater uncertainty.
-
-Min-max scaled range of (q0.975 - q0.025) indicates where greater weight (``w``) is placed,
-such that ``w=1+α`` for the most confident time period (i.e., lowest range) and ``w=2``
-for the least confident time period, where ``α`` is the minimum additional weight to apply
-(defaults to 0).
-
-Notes: Ignores leap days when using monthday periods.
-
-# Returns
-tuple: 
-     - x_section : Median of observations for a given period (default: day of year)
-     - low_section : Lower 95 percentile interval bound for a given period
-     - upp_section : Upper 95 percentile interval bound for a given period
-     - min_section : minimum score for a given period
-     - max_section : maximum score for a given period
-     - whisker_range : Range indicated by q0.975 - q0.025 (95 percentile interval)
-     - cv_r : coefficient of variation across whisker_range
-     - weighted : weights (1 to 2), with minimum weight of 1 + `min_weight`
-"""
-function temporal_uncertainty(dates, obs, sim; period::Function=monthday, func::Function=ME, min_weight::Float64=1.0)
-
-    x_section, low_section, upp_section, min_section, max_section, whisker_range, cv_r, std_error = temporal_uncertainty(dates, obs, sim, period, func)
-
-    # use min-max scaling to indicate where to place greater weight
-    max_range = max_section .- min_section
-    min_of_range = minimum(max_range)
-    weights = 1 .+ ((max_range .- min_of_range) ./ (maximum(max_range) - min_of_range))
-    weights[weights .< (1.0 + min_weight)] .= (1.0 + min_weight)
-
-    weighted = fill(1.0, length(obs))
-    doy = Dates.dayofyear.(dates)
-    mds = Dates.monthday.(dates)
-    for (idx, (d, md)) in enumerate(zip(doy, mds))
-        if md == (2, 29)
-            weighted[idx] = weights[d+1]
-            continue
-        elseif d == 366
-            weighted[idx] = weights[d-1]
-            continue
+function Base.getproperty(a::TemporalCrossSection, v::Symbol)
+    if v == :cross_section
+        if isnothing(getfield(a, :cross_section))
+            boxframe = cross_section(a)
+            return boxframe
         end
-
-        weighted[idx] = weights[d]
     end
 
-    return x_section, low_section, upp_section, min_section, max_section, whisker_range, cv_r, std_error, weighted
+    if v == :offsets
+        return offsets(a)
+    end
+
+    if in(v, [:cv_median, :cv_95, :cv_75, :mean_95, :mean_75, :std_75, :std_95])
+        boxframe = a.cross_section
+
+        if v == :cv_median
+            med = boxframe[:, :median]
+            cv_med = std(med) ./ mean(med)
+            return cv_med
+        elseif in(v, [:cv_95, :mean_95, :std_95])
+            upper = boxframe[:, :upper_95]
+            lower = boxframe[:, :lower_95]
+            wr = upper .- lower
+
+            if v == :cv_95
+                return std(wr) ./ mean(wr)
+            elseif v == :mean_95
+                return mean(wr)
+            elseif v == :std_95
+                return std(wr, corrected=false)
+            end
+        elseif in(v, [:cv_75, :mean_75, :std_75])
+            upper = boxframe[:, :upper_75]
+            lower = boxframe[:, :lower_75]
+            wr = upper .- lower
+
+            if v == :cv_75
+                return std(wr) ./ mean(wr)
+            elseif v == :mean_75
+                return mean(wr)
+            elseif v == :std_75
+                return std(wr, corrected=false)
+            end
+        end
+    else
+        return getfield(a, v)
+    end
 end
