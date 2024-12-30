c86ead0e4f41f0374ec66263f5a452e6ca82e3fd
Takuya Iwanaga <takuyai@gmail.com>
2024-12-31 01:00:46 +1100

    Clean up example
    Temporarily adding info statement to debug CI

--- examples/run_nodes.jl
+++ examples/run_nodes.jl
@@ -11,21 +11,30 @@ sn = load_network("Example Network", joinpath(data_path, "campaspe_network.yml")
 
 # Load climate data
 date_format = "YYYY-mm-dd"
-climate_data = CSV.File(joinpath(data_path, "climate/climate_historic.csv"),
-                          comment="#",
-                          dateformat=date_format) |> DataFrame
+climate_data = CSV.read(
+    joinpath(data_path, "climate/climate_historic.csv"),
+    DataFrame;
+    comment="#",
+    dateformat=date_format
+)
 
 dam_level_fn = joinpath(data_path, "gauges/406000_historic_levels_for_fit.csv")
 dam_releases_fn = joinpath(data_path, "gauges/406000_historic_outflow.csv")
-hist_dam_levels = CSV.File(dam_level_fn, dateformat=date_format) |> DataFrame
-hist_dam_releases = CSV.File(dam_releases_fn, dateformat=date_format) |> DataFrame
+
+@info dam_level_fn
+@info dam_releases_fn
+
+hist_dam_levels = CSV.read(dam_level_fn, DataFrame; dateformat=date_format)
+hist_dam_releases = CSV.read(dam_releases_fn, DataFrame; dateformat=date_format)
 
 rename!(hist_dam_releases, ["406000_outflow_[ML]" => "406000_releases_[ML]"])
 
 # Subset to same range
-climate_data, hist_dam_levels, hist_dam_releases = Streamfall.align_time_frame(climate_data,
-                                                                               hist_dam_levels,
-                                                                               hist_dam_releases)
+climate_data, hist_dam_levels, hist_dam_releases = Streamfall.align_time_frame(
+    climate_data,
+    hist_dam_levels,
+    hist_dam_releases
+)
 
 climate = Climate(climate_data, "_rain", "_evap")
 
